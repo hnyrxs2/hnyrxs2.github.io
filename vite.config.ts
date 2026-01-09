8330c066363b44d44a99da1d40e88aa53b513a23
Honey Rose Roxas <honeyrose.roxas@infor.com>
2026-01-09 20:10:56 +0800

    hello test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/', // correct for username.github.io
   plugins: [react()],
+    server: {
+    cors: {
+      // the origin you will be accessing via browser
+      origin: 'https://hnyrxs2.github.io',
+    },
+  },
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    },
+  },
 });
